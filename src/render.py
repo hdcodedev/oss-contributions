4d197d71fbc0b379563eae97c3aab672113af935
hdcode.dev <215285761+hdcodedev@users.noreply.github.com>
2026-09-24 13:18:51 +0200

    style: show only the clickable icon in the stats Project column

--- src/render.py
+++ src/render.py
@@ -21,8 +21,9 @@ def generate_markdown(contributions_by_date, featured_repos, output_file="README
             for project in stats['projects']:
                 repo_display = (
                     f"<a href=\"{project['repo_url']}\">"
-                    f"<img src=\"{project['logo_url']}\" width=\"24\" height=\"24\" style=\"vertical-align:middle;\"/>"
-                    f"</a> {project['name']}"
+                    f"<img src=\"{project['logo_url']}\" width=\"24\" height=\"24\" style=\"vertical-align:middle;\" "
+                    f"title=\"{project['name']}\" alt=\"{project['name']}\"/>"
+                    "</a>"
                 )
                 cells = [str(project['categories'].get(c['category'], 0)) for c in stats['columns']]
                 f.write(f"| {repo_display} | {' | '.join(cells)} | **{project['total']}** |\n")
